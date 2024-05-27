5c6a443ae3f3f10db0cfeb329b23ae069c6c8186
Funobu / Futa Matsuo <70759129+funobu@users.noreply.github.com>
2024-05-27 16:13:16 +0900

    chore: prettierのフォーマット設定

--- frontend/src/shared/hooks/index.ts
+++ frontend/src/shared/hooks/index.ts
@@ -1,6 +1,6 @@
-import { useDispatch, useSelector, TypedUseSelectorHook } from 'react-redux';
-import { RootState, AppDispatch } from '../store';
+import { useDispatch, useSelector, TypedUseSelectorHook } from 'react-redux'
+import { RootState, AppDispatch } from '../store'
 
-export const useAppDispatch = () => useDispatch<AppDispatch>();
+export const useAppDispatch = () => useDispatch<AppDispatch>()
 
-export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
+export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector
