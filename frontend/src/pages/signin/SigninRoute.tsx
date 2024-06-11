002d77549f651dda4806295d20b99391eb53e21c
takeru-a-wada <takeru.a.wada@dena.com>
2024-06-11 19:54:54 +0900

    ログインフォームの仮実装

--- frontend/src/pages/signin/SigninRoute.tsx
+++ frontend/src/pages/signin/SigninRoute.tsx
@@ -1,32 +1,77 @@
-import { Card, CardBody, CardHeader, Container, Text, HStack, Heading, CardFooter, VStack } from '@yamada-ui/react'
-import { useEffect, useState } from 'react'
-import { MOCK_POSTS, post } from '../../shared/models'
-import dayjs from 'dayjs'
+import { useState } from 'react'
+import { Button, Container, FormControl, Input } from '@yamada-ui/react'
 
 export const SigninRoute = () => {
-  const [posts, setPosts] = useState<Array<post>>(MOCK_POSTS)
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+  const [usernameError, setUsernameError] = useState('')
+  const [passwordError, setPasswordError] = useState('')
+
+  const validateUsername = (value: string) => {
+    if (value === '') {
+      setUsernameError('ユーザー名を入力してください。')
+      return
+    }
+    const regex = /^[a-zA-Z0-9]+$/
+    if (!regex.test(value)) {
+      setUsernameError('ユーザー名は英数のみ許可されます。記号は使用できません。')
+    } else {
+      setUsernameError('')
+    }
+  }
+
+  const validatePassword = (value: string) => {
+    if (value === '') {
+      setPasswordError('パスワードを入力してください。')
+      return
+    }
+    const regex = /^[\x20-\x7E]+$/ // ASCII範囲の印刷可能な文字
+    if (!regex.test(value)) {
+      setPasswordError('パスワードはASCII範囲の英数記号のみ許可されます。')
+    } else {
+      setPasswordError('')
+    }
+  }
+
+  const handleUsernameChange = (event: { target: { value: string } }) => {
+    const value = event.target.value
+    setUsername(value)
+    validateUsername(value)
+  }
+
+  const handlePasswordChange = (event: { target: { value: string } }) => {
+    const value = event.target.value
+    setPassword(value)
+    validatePassword(value)
+  }
+
   return (
     <Container>
-      <Heading size="lg">投稿一覧</Heading>
-      <VStack w="full">
-        {posts.map((post) => (
-          <Card key={post.id} variant="outline" w="full">
-            <CardHeader>
-              <Heading size="md">{post.title}</Heading>
-            </CardHeader>
+      <FormControl label="ユーザー名" isRequired isInvalid={usernameError !== ''} errorMessage={usernameError}>
+        <Input
+          type="text"
+          placeholder="ユーザー名を入力してください。"
+          value={username}
+          onChange={handleUsernameChange}
+        />
+      </FormControl>
 
-            <CardBody>
-              <Text>{post.body}</Text>
-            </CardBody>
-            <CardFooter>
-              <HStack>
-                <Text>{post.userName}</Text>
-                <Text>更新日時： {dayjs(post.updatedAt).format('YYYY年M月D日 HH:mm:ss')}</Text>
-              </HStack>
-            </CardFooter>
-          </Card>
-        ))}
-      </VStack>
+      <FormControl label="パスワード" isRequired isInvalid={passwordError !== ''} errorMessage={passwordError}>
+        <Input
+          type="password"
+          placeholder="パスワードを入力してください。"
+          value={password}
+          onChange={handlePasswordChange}
+        />
+      </FormControl>
+
+      <Button
+        onClick={() => {
+          // TODO: エラーがない場合にサインインAPIを叩き、ルーティング
+        }}
+      >
+        サインイン
+      </Button>
     </Container>
   )
 }
