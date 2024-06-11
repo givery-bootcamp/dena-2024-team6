e26977e2d778323ae7083d7ecb480fdb284e5dd1
Hiroki Tokunaga <hiroki.tokunaga@dena.com>
2024-06-11 13:50:53 +0900

    エンドポイントを生やす

--- backend/internal/middleware/router.go
+++ backend/internal/middleware/router.go
@@ -13,4 +13,7 @@ func SetupRoutes(app *gin.Engine) {
 	app.GET("/hello", controllers.HelloWorld)
 	app.GET("/posts", controllers.PostsController)
 	app.GET("/posts/:postid", controllers.PostDetailController)
+	app.POST("/signin", func(ctx *gin.Context) {
+		ctx.String(200, "Signin")
+	})
 }
