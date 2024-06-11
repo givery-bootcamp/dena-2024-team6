c0d2349b2bdbddcc6629c559c4020436dee58eb3
Hiroki Tokunaga <hiroki.tokunaga@dena.com>
2024-06-11 13:55:27 +0900

    JSON形式の値を受け取り単に返す

--- backend/internal/middleware/router.go
+++ backend/internal/middleware/router.go
@@ -14,6 +14,18 @@ func SetupRoutes(app *gin.Engine) {
 	app.GET("/posts", controllers.PostsController)
 	app.GET("/posts/:postid", controllers.PostDetailController)
 	app.POST("/signin", func(ctx *gin.Context) {
-		ctx.String(200, "Signin")
+		// Get two parameters from JSON: username (string) and password (string)
+
+		var json struct {
+			Username string `json:"username" binding:"required"`
+			Password string `json:"password" binding:"required"`
+		}
+
+		if err := ctx.ShouldBindJSON(&json); err != nil {
+			ctx.JSON(400, gin.H{"error": err.Error()})
+			return
+		}
+
+		ctx.JSON(200, gin.H{"username": json.Username, "password": json.Password})
 	})
 }
