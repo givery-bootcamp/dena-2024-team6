7d5b48c89a78df8593064d26b4cf800863119921
shohei-u <shohei.u@dena.com>
2024-06-17 16:48:43 +0900

    ✨ signin userの確認

--- backend/internal/controllers/user_controller.go
+++ backend/internal/controllers/user_controller.go
@@ -2,8 +2,10 @@ package controllers
 
 import (
 	"errors"
+	"fmt"
 	"myapp/internal/repositories"
 	"myapp/internal/usecases"
+	"reflect"
 
 	"github.com/gin-gonic/gin"
 	"github.com/golang-jwt/jwt"
@@ -15,32 +17,33 @@ func UserController(ctx *gin.Context) {
 
 	token, err := ctx.Cookie("token")
 	if err != nil {
-		handleError(ctx, 401, errors.New("unauthorized"))
+		fmt.Println(err)
+		handleError(ctx, 401, errors.New("unauthorized1"))
 		return
 	}
 
-	secret := []byte("secret")
+	secret := []byte("my_secret_key")
 	claims, err := jwt.Parse(token, func(token *jwt.Token) (interface{}, error) {
 		return secret, nil
 	})
 	if err != nil {
-		handleError(ctx, 401, errors.New("unauthorized"))
+		handleError(ctx, 401, errors.New("unauthorized2"))
 		return
 	}
 
 	claimsMap, ok := claims.Claims.(jwt.MapClaims)
 	if !ok {
-		handleError(ctx, 401, errors.New("unauthorized"))
+		handleError(ctx, 401, errors.New("unauthorized3"))
 		return
 	}
 
-	userID, ok := claimsMap["ID"].(int)
+	userID, ok := claimsMap["ID"].(float64)
 	if !ok {
-		handleError(ctx, 401, errors.New("unauthorized"))
+		fmt.Println(reflect.TypeOf(claimsMap["ID"]))
+		handleError(ctx, 401, errors.New("unauthorized4"))
 		return
 	}
-
-	result, err := usecase.Execute(userID)
+	result, err := usecase.Execute(int(userID))
 	if err != nil {
 		handleError(ctx, 500, err)
 		return
--- backend/internal/repositories/user_repository.go
+++ backend/internal/repositories/user_repository.go
@@ -16,9 +16,19 @@ func NewUserRepository(conn *gorm.DB) *UserRepository {
 	}
 }
 
-func (r *UserRepository) Get() (*entities.User, error) {
+func (r *UserRepository) Get(id int) (*entities.User, error) {
+	var user User
+	err := r.Conn.Table("users").Select(`
+		id,
+		name
+	`).Where("id = ?", id).First(&user).Error
+
+	if err != nil {
+		return nil, err
+	}
+
 	return &entities.User{
-		ID:       42,
-		Username: "Yakisoba Taro",
+		ID:       user.ID,
+		Username: user.Name,
 	}, nil
 }
