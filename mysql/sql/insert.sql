529288b50eeed1ee6ec039e2474940c2192377e8
shohei-u <shohei.u@dena.com>
2024-06-18 17:06:51 +0900

    ✨ passwordのハッシュ化

--- mysql/sql/insert.sql
+++ mysql/sql/insert.sql
@@ -1,8 +1,8 @@
 INSERT INTO hello_worlds (lang, message) VALUES ('en', 'Hello World');
 INSERT INTO hello_worlds (lang, message) VALUES ('ja', 'こんにちは 世界');
 
-INSERT INTO users (name, password) VALUES ('taro', 'password');
-INSERT INTO users (name, password) VALUES ('hanako', 'PASSWORD');
+INSERT INTO users (name, password) VALUES ('taro', '5e884898da28047151d0e56f8dc6292773603d0d6aabbdd62a11ef721d1542d8');
+INSERT INTO users (name, password) VALUES ('hanako', '0be64ae89ddd24e225434de95d501711339baeee18f009ba9b4369af27d30d60');
 
 INSERT INTO posts (user_id, title, body) VALUES (1, 'test1', '質問1\n改行');
 INSERT INTO posts (user_id, title, body) VALUES (1, 'test2', '質問2\n改行');
