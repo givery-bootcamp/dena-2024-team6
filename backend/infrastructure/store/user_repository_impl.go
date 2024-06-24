09dbba4819bd2954915efc5238846b21bbbeb536
futa-a-matsuo <futa.a.matsuo@dena.com>
2024-06-24 11:10:33 +0900

    feat: UserRepositoryのGetByUserNameAndPasswordを実装

---
+++ backend/infrastructure/store/user_repository_impl.go
@@ -0,0 +1,51 @@
+package store
+
+import (
+	"context"
+	"database/sql"
+	"errors"
+	"log"
+	"myapp/domain/model"
+	"myapp/domain/repository"
+	"myapp/infrastructure/store/dao"
+
+	"github.com/jmoiron/sqlx"
+	"github.com/samber/do"
+)
+
+type UserRepositoryImpl struct {
+	db *sqlx.DB
+}
+
+func NewUserRepositoryImpl(i *do.Injector) (repository.UserRepository, error) {
+	db := do.MustInvoke[*sqlx.DB](i)
+	return UserRepositoryImpl{
+		db: db,
+	}, nil
+}
+
+// GetByUserNameAndPassword implements repository.UserRepository.
+func (u UserRepositoryImpl) GetByUserNameAndPassword(ctx context.Context, userName string, password string) (model.User, error) {
+	user := dao.UserTable{}
+	if err := u.db.GetContext(ctx, &user, `
+		SELECT
+			id,
+			name,
+			password
+		FROM
+			users
+		WHERE
+			name=?
+		AND
+			password=?
+	`, userName, password); err != nil {
+		if errors.Is(err, sql.ErrNoRows) {
+			return model.User{}, nil
+		}
+		log.Println(err)
+
+		return model.User{}, err
+	}
+
+	return dao.ConvertUserTableToDomainUser(user), nil
+}
