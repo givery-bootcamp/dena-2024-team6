1c6e4a41a02f8f1af68c039f2de637abbbd94e8f
futa-a-matsuo <futa.a.matsuo@dena.com>
2024-06-25 17:05:52 +0900

    feat: DAOからドメインモデルに変換する処理追加&UserNameのカラム追加

--- backend/infrastructure/store/dao/comment.go
+++ backend/infrastructure/store/dao/comment.go
@@ -1,12 +1,28 @@
 package dao
 
-import "time"
+import (
+	"myapp/domain/model"
+	"time"
+)
 
 type CommentTable struct {
 	ID        int       `db:"id"`
 	UserID    int       `db:"user_id"`
+	UserName  string    `db:"user_name"`
 	PostID    int       `db:"post_id"`
 	Body      string    `db:"body"`
 	CreatedAt time.Time `db:"created_at"`
 	UpdatedAt time.Time `db:"updated_at"`
+}
+
+func ConvertCommentTableToDomainComment(ct CommentTable) model.Comment {
+	return model.Comment{
+		ID:        ct.ID,
+		PostID:    ct.PostID,
+		UserID:    ct.UserID,
+		UserName:  ct.UserName,
+		Body:      ct.Body,
+		CreatedAt: ct.CreatedAt,
+		UpdatedAt: ct.UpdatedAt,
+	}
 }
