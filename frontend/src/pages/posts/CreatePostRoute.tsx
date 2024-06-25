fc7147fbaee80b00d0b0b17d762937f5c3d33d09
shohei-u <shohei.u@dena.com>
2024-06-25 14:36:45 +0900

    ✨ 投稿作成

--- frontend/src/pages/posts/CreatePostRoute.tsx
+++ frontend/src/pages/posts/CreatePostRoute.tsx
@@ -1,15 +1,77 @@
 import { useState } from 'react'
-import { Button, Container, Divider, FormControl, HStack, Heading, Input } from '@yamada-ui/react'
+import { useNavigate } from 'react-router-dom'
+import { Button, Container, Divider, FormControl, HStack, Heading, Input,useSnacks } from '@yamada-ui/react'
 import MarkdownEditor from '@uiw/react-markdown-editor'
 import { Link } from 'react-router-dom'
+import { usePostPosts } from '../../api/api'
 
 export const CreatePostRoute = () => {
   const [title, setTitle] = useState('')
   const [content, setContent] = useState('')
 
+  const {snack} = useSnacks()
+
+  const navigate = useNavigate()
+  const {mutate} = usePostPosts()
+
+  const validdateTitle = (value: string) => {
+    if (value === '') {
+      snack({
+        status: 'error',
+        description: 'タイトルを入力してください。'
+      })
+      return false
+    }
+    if (value.length > 100) {
+      snack({
+        status: 'error',
+        description: 'タイトルは100文字以内で入力してください。'
+      })
+      return false
+    }
+    return true
+  }
+
+  const validateContent = (value: string) => {
+    if (value === '') {
+      snack({
+        status: 'error',
+        description: '内容を入力してください。'
+      })
+      return false
+    }
+    return true
+  }
+
   const handleSubmit = (event: { preventDefault: () => void }) => {
     event.preventDefault()
+    const isTitleValid = validdateTitle(title)
+    const isContentValid = validateContent(content)
+
+    if (isTitleValid && isContentValid) {
+      mutate(
+        {data:{title:title, body:content}},
+        {
+          onSuccess: () => {
+            snack({
+              status: 'success',
+              description: '投稿に成功しました。'
+            
+            })
+            navigate('/')
+          },
+          onError: () => {
+            snack(
+              {
+                status: 'error',
+                description: '投稿に失敗しました。'
+              }
+            )
+          }
+        }
+      )
     console.log('submit', { title, content })
+    }
   }
 
   return (
@@ -34,7 +96,7 @@ export const CreatePostRoute = () => {
               キャンセル
             </Button>
           </Link>
-          <Button type="submit" colorScheme="primary">
+          <Button onClick={handleSubmit} type="submit" colorScheme="primary">
             投稿する
           </Button>
         </HStack>
