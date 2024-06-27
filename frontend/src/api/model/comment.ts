d8f2c132813603bf6b5afd37eb0d7fdc3640776c
shohei-u <shohei.u@dena.com>
2024-06-27 12:08:51 +0900

    ✨ comment type

--- frontend/src/api/model/comment.ts
+++ frontend/src/api/model/comment.ts
@@ -2,6 +2,7 @@ export interface Comment {
     id: number
     post_id: number
     user_id: number
+    user_name: string
     body: string
     created_at: string
     updated_at: string
@@ -13,13 +14,11 @@ export interface CommentList {
 
 export interface CreatComment{
     post_id: number
-    user_id: number
     body: string
 }
 
 export interface UpdateComment{
     id: number
     post_id: number
-    user_id: number
     body: string
 }
