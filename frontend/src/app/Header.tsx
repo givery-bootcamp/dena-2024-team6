38eb5febd2d22e43f79060b609375d6ce71b32f5
takeru-a-wada <takeru.a.wada@dena.com>
2024-06-27 11:40:45 +0900

    isFetchingでローディング処理追加

--- frontend/src/app/Header.tsx
+++ frontend/src/app/Header.tsx
@@ -1,11 +1,11 @@
-import { Box, Button, Text } from '@yamada-ui/react'
+import { Box, Button, Loading, Text } from '@yamada-ui/react'
 import { useNavigate } from 'react-router-dom'
 import { useEffect, useState } from 'react'
 import { usePostSignout, useGetUser } from '../api/api'
 
 export const Header = () => {
   const navigate = useNavigate()
-  const { data: user, isError } = useGetUser()
+  const { data: user, isError, refetch, isFetching } = useGetUser()
   const [currentUser, setCurrentUser] = useState(user)
 
   const { mutate: signoutMutate } = usePostSignout({
@@ -29,12 +29,21 @@ export const Header = () => {
     <header className="app-header">
       サンプルアプリケーション
       <Box display="flex" alignItems="center">
-        {isError || !currentUser ? (
+        {isFetching ? (
+          <Loading variant="circles" size="6xl" color="cyan.500" />
+        ) : isError || !currentUser ? (
           <Button onClick={() => signoutMutate()}>サインイン</Button>
         ) : (
           <>
             <Text mr={2}>{currentUser.user_name}</Text>
-            <Button onClick={() => signoutMutate()}>サインアウト</Button>
+            <Button
+              onClick={() => {
+                signoutMutate()
+                refetch()
+              }}
+            >
+              サインアウト
+            </Button>
           </>
         )}
       </Box>
