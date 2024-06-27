c3976ee3b19171cfc60b4eb8e061c0b669582f97
takeru-a-wada <takeru.a.wada@dena.com>
2024-06-27 17:31:17 +0900

    ルーティング設定

--- frontend/src/app/AppRoute.tsx
+++ frontend/src/app/AppRoute.tsx
@@ -1,16 +1,18 @@
 import { Routes, Route } from 'react-router-dom'
 
 import { PostsRoute } from '../pages/posts/PostsRoute'
-import { SigninRoute } from '../pages/signin/SigninRoute'
 import { PostDetailRoute } from '../pages/posts/detail/PostDetailRoute'
 import { CreatePostRoute } from '../pages/posts/CreatePostRoute'
 import { UpdatePostRoute } from '../pages/posts/update/UpdatePostRoute'
+import { SigninRoute } from '../pages/signin/SigninRoute'
+import { SignupRoute } from '../pages/signup/SignupRoute'
 
 export const AppRoute = () => {
   return (
     <Routes>
-      <Route path="/signin" element={<SigninRoute />} />
       <Route path="/" element={<PostsRoute />} />
+      <Route path="/signin" element={<SigninRoute />} />
+      <Route path="/signup" element={<SignupRoute />} />
       <Route path="/posts/:id" element={<PostDetailRoute />} />
       <Route path="/posts/:id/edit" element={<UpdatePostRoute />} />
       <Route path="/posts/new" element={<CreatePostRoute />} />
