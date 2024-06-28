40e890899bcd20bb4784d8871f274d1a184f5110
futa-a-matsuo <futa.a.matsuo@dena.com>
2024-06-28 14:41:30 +0900

    fix: フォントだけindex.scssから読み込むようにした

--- frontend/src/app/App.tsx
+++ frontend/src/app/App.tsx
@@ -1,10 +1,13 @@
 import { AppRoute } from './AppRoute'
 import { Box } from '@yamada-ui/react'
+import './App.scss'
 
 function App() {
   return (
     <Box w="100vw" minH="100vh" bgColor="#98C9DE">
-      <AppRoute />
+      <div className="app-body">
+        <AppRoute />
+      </div>
     </Box>
   )
 }
