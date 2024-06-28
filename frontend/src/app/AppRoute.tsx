8c41a45de2ed75e1efec7196831d744f5d1d824f
takeru-a-wada <takeru.a.wada@dena.com>
2024-06-28 18:57:49 +0900

    modalとして切り分け

--- frontend/src/app/AppRoute.tsx
+++ frontend/src/app/AppRoute.tsx
@@ -3,7 +3,7 @@ import { Routes, Route } from 'react-router-dom'
 import { PostsRoute } from '../pages/posts/PostsRoute'
 import { SigninRoute } from '../pages/signin/SigninRoute'
 import { PostDetailRoute } from '../pages/posts/detail/PostDetailRoute'
-import { CreatePostRoute } from '../pages/posts/CreatePostRoute'
+// import { CreatePostRoute } from '../pages/posts/CreatePostRoute'
 import { UpdatePostRoute } from '../pages/posts/update/UpdatePostRoute'
 
 export const AppRoute = () => {
@@ -13,7 +13,6 @@ export const AppRoute = () => {
       <Route path="/" element={<PostsRoute />} />
       <Route path="/posts/:id" element={<PostDetailRoute />} />
       <Route path="/posts/:id/edit" element={<UpdatePostRoute />} />
-      <Route path="/posts/new" element={<CreatePostRoute />} />
     </Routes>
   )
 }
