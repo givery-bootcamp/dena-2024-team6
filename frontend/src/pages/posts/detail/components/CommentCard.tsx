913e35657371e47743c355fa5385efef7f9977f5
futa-a-matsuo <futa.a.matsuo@dena.com>
2024-06-28 14:09:41 +0900

    feat: コメントカードのコンポーネント実装

---
+++ frontend/src/pages/posts/detail/components/CommentCard.tsx
@@ -0,0 +1,29 @@
+import { Text, Avatar, Box, Flex, HStack, SkeletonCircle } from '@yamada-ui/react'
+import { memo } from 'react'
+import dayjs from 'dayjs'
+
+type CommentCardProps = {
+  userName?: string
+  userIconURL?: string
+  body?: string
+  createdAt?: Date
+}
+
+export const CommentCard = memo(function ({ userName, userIconURL, body, createdAt }: CommentCardProps) {
+  return (
+    <Flex w="full" borderRadius="lg" gap="md">
+      {userName ? <Avatar size="sm" name={userName} src={userIconURL} /> : <SkeletonCircle w="32px" h="28px" />}
+      <Flex gap="sm" flexDir="column">
+        <HStack gap="lg">
+          <Text color="#ffffff">Funobu</Text>
+          <Text fontSize="sm" color="neutral.50">
+            {dayjs(createdAt).format('YYYY年M月D日 hh:mm s秒')}
+          </Text>
+        </HStack>
+        <Text color="#ffffff" fontSize="md">
+          {body}
+        </Text>
+      </Flex>
+    </Flex>
+  )
+})
