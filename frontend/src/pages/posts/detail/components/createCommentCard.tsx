5032f968e7819a77375bb40e7e9243eb8214d9f3
takeru-a-wada <takeru.a.wada@dena.com>
2024-06-28 20:11:38 +0900

    バリデーションの削除とPropsの追加

--- frontend/src/pages/posts/detail/components/createCommentCard.tsx
+++ frontend/src/pages/posts/detail/components/createCommentCard.tsx
@@ -1,73 +1,61 @@
 import { useCreatePostComments } from '@api/hooks'
-import { Text, Box, Button, Divider, Flex, HStack, Icon, IconButton, Input, useSnacks } from '@yamada-ui/react'
+import { Box, Button, Flex, HStack, Icon, IconButton, Input, useSnacks } from '@yamada-ui/react'
 import { MessageSquareText, Heart } from 'lucide-react'
 import { useState } from 'react'
 
-export const CreatePostCommentCard = ({ id }: { id: number }) => {
-  const [body, setBody] = useState('')
-  const [bodyError, setBodyError] = useState<string | null>(null)
-  const { mutate } = useCreatePostComments()
+interface CreatePostCommentCardProps {
+  id: number
+  onSuccess: () => void
+}
 
+export const CreatePostCommentCard = ({ id, onSuccess }: CreatePostCommentCardProps) => {
+  const [body, setBody] = useState('')
+  const { mutate } = useCreatePostComments()
   const { snack } = useSnacks()
 
-  const validdateBody = (value: string) => {
-    if (value === '') {
-      setBodyError('コメントを入力してください。')
-      return false
-    }
-    setBodyError(null)
-    return true
-  }
-
   const handleBodyChange = (event: { target: { value: string } }) => {
-    const value = event.target.value
-    setBody(value)
-    validdateBody(value)
+    setBody(event.target.value)
   }
 
   function handleSubmit(event: { preventDefault: () => void }): void {
     event.preventDefault()
-    const isBodyValid = validdateBody(body)
-    if (isBodyValid) {
-      mutate(
-        { postId: String(id), data: { body: body } },
-        {
-          onSuccess: () => {
-            setBody('')
-            snack({
-              status: 'success',
-              description: '投稿に成功しました。'
-            })
-          },
-          onError: () => {
-            snack({
-              status: 'error',
-              description: '投稿に失敗しました。'
-            })
-          }
-        }
-      )
+    const trimmedBody = body.trim()
+    if (trimmedBody === '') {
+      snack({
+        status: 'error',
+        description: 'コメントを入力してください。'
+      })
+      return
     }
-    return
+    mutate(
+      { postId: String(id), data: { body: trimmedBody } },
+      {
+        onSuccess: () => {
+          setBody('')
+          snack({
+            status: 'success',
+            description: 'コメントの投稿に成功しました。'
+          })
+          onSuccess() // コメント投稿成功後に親コンポーネントで指定された関数を呼び出す
+        },
+        onError: () => {
+          snack({
+            status: 'error',
+            description: 'コメントの投稿に失敗しました。'
+          })
+        }
+      }
+    )
   }
 
   return (
     <Flex px="md" py="lg" justifyContent="space-between">
-      <Box
-        w="75vw"
-        p="sm"
-        bgColor="whiteAlpha.800"
-        borderRadius="md"
-        _hover={{
-          bgColor: 'neutral.100'
-        }}
-      >
+      <Box w="75vw" p="sm" bgColor="whiteAlpha.800" borderRadius="md" _hover={{ bgColor: 'neutral.100' }}>
         <form onSubmit={handleSubmit}>
           <HStack>
             <Icon size="lg" as={MessageSquareText} />
             <Input type="text" placeholder="コメントを入力してください" value={body} onChange={handleBodyChange} />
             <Button
-              onClick={handleSubmit}
               color="White"
               bgGradient="linear(to-r, #00D1FF,#8ABBF5, #DEC9EB)"
               fontWeight="bold"
