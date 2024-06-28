6b47463c1e65f1b5eb9c70fc64179f21fc0a284f
rim0o8 <kurashimayuuri@gmail.com>
2024-06-28 16:24:05 +0900

    fix: raise error when like unexist post

--- backend/application/like_post_usecase.go
+++ backend/application/like_post_usecase.go
@@ -19,21 +19,30 @@ type LikePostUsecaseInput struct {
 }
 
 func NewLikePostUsecase(i *do.Injector) (LikePostUsecase, error) {
+	postRepo := do.MustInvoke[repository.PostRepository](i)
 	likeRepo := do.MustInvoke[repository.LikeRepository](i)
 	return &LikePostUsecaseInteractor{
+		postRepository: postRepo,
 		likeRepository: likeRepo,
 	}, nil
 }
 
 type LikePostUsecaseInteractor struct {
+	postRepository repository.PostRepository
 	likeRepository repository.LikeRepository
 }
 
 // Execute implements LikePostUsecase.
 func (c *LikePostUsecaseInteractor) Execute(ctx context.Context, input LikePostUsecaseInput) error {
+	post, _ := c.postRepository.GetDetail(ctx, input.PostID)
+	if post.IsEmpty() {
+		return apperror.New(apperror.CodeNotFound, "post is not found")
+	}
+
 	likes, err := c.likeRepository.Get(ctx, input.PostID)
 	if err == sql.ErrNoRows {
 		_, err = c.likeRepository.Create(ctx, input.PostID)
+		likes = 0
 		if err != nil {
 			return apperror.New(apperror.CodeInternalServer, "failed to create likes record")
 		}
