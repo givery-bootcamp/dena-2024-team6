7b7b9c283d0eed5c18752f82631132c6041e154a
takeru-a-wada <takeru.a.wada@dena.com>
2024-06-28 21:40:42 +0900

    providerの作成

---
+++ frontend/src/shared/provider/UserProvider.tsx
@@ -0,0 +1,28 @@
+// context/UserContext.tsx
+import { User } from '@shared/models/user'
+import React, { createContext, useContext, useState, ReactNode } from 'react'
+
+type UserContextType = {
+  currentUser: User | null
+  setCurrentUser: React.Dispatch<React.SetStateAction<User | null>>
+}
+
+const UserContext = createContext<UserContextType | null>(null)
+
+interface UserProviderProps {
+  children: ReactNode
+}
+
+export const UserProvider: React.FC<UserProviderProps> = ({ children }) => {
+  const [currentUser, setCurrentUser] = useState<User | null>(null)
+
+  return <UserContext.Provider value={{ currentUser, setCurrentUser }}>{children}</UserContext.Provider>
+}
+
+export const useUser = () => {
+  const context = useContext(UserContext)
+  if (!context) {
+    throw new Error('useUser must be used within a UserProvider')
+  }
+  return context
+}
